799dcbc2f9c12a3e6d72856bda5e59d843a2394b
kelvinlopez1 <kelvinlopez060301@gmail.com>
2023-08-28 11:41:44 -0400

    cambios en la practicas aprendiendo como ocultar y mostrar datos en javascritp

---
+++ practicas.js
@@ -0,0 +1,21 @@
+/*var x = document.getElementById("txt_nunmer");
+let y = document.getElementById("txt_nunmer2");
+let btn_sumar = document.getElementById("sumar");
+
+
+function suma(num1, num2){
+ return(num1 + num2);
+}
+
+btn_sumar.addEventListener("click", function(){
+   console.log(`el resultado de ${x.value} + ${y.value} es ${suma(x.value, y.value)}`);
+});
+*/
+//let pulsar = document.getElementById("click");
+
+
+
+
+
+
+
